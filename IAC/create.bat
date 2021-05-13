77de00e73d16c8e272c588d47f2960c3412b8119
Leena Bhavnani <61199820+leenabhavnani@users.noreply.github.com>
2021-05-13 19:15:15 +0530

    network section

--- IAC/create.bat
+++ IAC/create.bat
@@ -1,1 +1,4 @@
 aws cloudformation create-stack --stack-name myInfraStack --template-body file://myInfra.yaml  --parameters file://myInfraParameter.json --region=us-west-2
+
+
+aws cloudformation create-stack --stack-name myPrivateNetworkStack --template-body file://private-network.yaml  --parameters file://myPrivateNetwork.json --region=us-west-2

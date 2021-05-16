672d8eff921d870d0944a73bf38f19dda9657983
Leena Bhavnani <61199820+leenabhavnani@users.noreply.github.com>
2021-05-16 19:08:47 +0530

    challenge 2

--- IAC/IACCreate.bat
+++ IAC/IACCreate.bat
@@ -1,3 +1,5 @@
 Rem aws cloudformation create-stack --stack-name myIACStack --template-body file://IACChallenge2.yaml  --parameters file://IACChallenge2Parameter.json --region=us-west-2
 
 aws cloudformation update-stack --stack-name myIACStack --template-body file://IACChallenge2.yaml  --parameters file://IACChallenge2Parameter.json --region=us-west-2
+
+aws cloudformation create-stack --stack-name myIAC3Stack --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --template-body file://IACChallenge3.yaml  --parameters file://IACChallenge3Parameter.json --region=us-west-2
